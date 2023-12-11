30ed1bc6afca31fd2ae7af46c5744e8a6863c3a1
brequet <requet.baptiste@gmail.com>
2023-12-11 13:53:58 +0100

    day 11.1

--- 2023/day-11/1/main.go
+++ 2023/day-11/1/main.go
@@ -18,19 +18,124 @@ func main() {
 	fmt.Printf("Result %d (expected %d)\n", result, EXPECTED_RESULT)
 }
 
+type Galaxy struct {
+	row int
+	col int
+}
+
 func solve(input []string) int {
 	grid := createGridUniverse(input)
 	printMat(grid)
-	return 0
+
+	galaxies := []Galaxy{}
+	for y, row := range grid {
+		for x, b := range row {
+			if b == '#' {
+				galaxies = append(galaxies, Galaxy{row: y, col: x})
+			}
+		}
+	}
+
+	sumShortest := 0
+	for _, galaxy := range galaxies {
+		// closest := findClosest(galaxies, galaxy)
+		for _, g2 := range galaxies {
+			sumShortest += distance(galaxy, g2)
+		}
+		// fmt.Println("Closest to", galaxy, "is", closest)
+	}
+
+	return sumShortest / 2
+}
+
+func findClosest(galaxies []Galaxy, galaxy Galaxy) int {
+	minDistance := -1
+	for _, g := range galaxies {
+		distance := distance(galaxy, g)
+		if distance > 0 {
+			if minDistance < 0 || distance < minDistance {
+				minDistance = distance
+			}
+		}
+	}
+	return minDistance
+}
+
+func distance(g1, g2 Galaxy) int {
+	return abs(g2.row-g1.row) + abs(g2.col-g1.col)
+}
+
+func abs(n int) int {
+	if n < 0 {
+		return -n
+	} else {
+		return n
+	}
 }
 
 func createGridUniverse(input []string) [][]byte {
-	grid := make([][]byte, len(input))
+	// init
+	grid := [][]byte{}
 	for i := range input {
-		grid[i] = []byte(input[i])
+		row := []byte(input[i])
+		grid = append(grid, row)
 	}
-	// TODO expand if necessary
-	return grid
+
+	// expand rows
+	emptyRowIndexes := []int{}
+	for i := range grid {
+		if isEmptyRow(grid, i) {
+			emptyRowIndexes = append(emptyRowIndexes, i)
+		}
+	}
+	for _, i := range utils.ReverseList(emptyRowIndexes) {
+		emptyRow := grid[i]
+		if len(grid) == i {
+			grid = append(grid, emptyRow)
+		} else {
+			tmpGrid := append(grid[:i], emptyRow)
+			grid = append(tmpGrid, grid[i:]...)
+		}
+	}
+
+	// expand cols
+	emptyColIndexes := []int{}
+	for j := range grid[0] {
+		if isEmptyCol(grid, j) {
+			emptyColIndexes = append(emptyColIndexes, j)
+		}
+	}
+	newGrid := [][]byte{}
+	for _, row := range grid {
+		newRow := []byte{}
+		for j, b := range row {
+			newRow = append(newRow, b)
+			if utils.Contains(emptyColIndexes, j) {
+				newRow = append(newRow, b)
+			}
+		}
+		newGrid = append(newGrid, newRow)
+	}
+
+	return newGrid
+}
+
+func isEmptyRow(mat [][]byte, rowIndex int) bool {
+	for _, b := range mat[rowIndex] {
+		if b != '.' {
+			return false
+		}
+	}
+	return true
+}
+
+func isEmptyCol(mat [][]byte, colIndex int) bool {
+	for _, row := range mat {
+		if row[colIndex] != '.' {
+			return false
+		}
+	}
+	return true
 }
 
 func printMat(mat [][]byte) {
--- 2023/day-11/2/main.go
+++ 2023/day-11/2/main.go
@@ -6,14 +6,147 @@ import (
 )
 
 const (
-	DAY             = "06"
-	EXPECTED_RESULT = 32583852
+	DAY             = "11"
+	EXPECTED_RESULT = 8410
 )
 
 func main() {
 	input := utils.GetInputAsSlice(fmt.Sprintf("./2023/day-%s/input", DAY))
 	fmt.Printf("Input of size: %d\n", len(input))
 
-	result := 0
+	result := solve(input)
 	fmt.Printf("Result %d (expected %d)\n", result, EXPECTED_RESULT)
+}
+
+type Galaxy struct {
+	row int
+	col int
+}
+
+func solve(input []string) int {
+	grid := createGridUniverse(input)
+	// printMat(grid)
+
+	galaxies := []Galaxy{}
+	for y, row := range grid {
+		for x, b := range row {
+			if b == '#' {
+				galaxies = append(galaxies, Galaxy{row: y, col: x})
+			}
+		}
+	}
+
+	sumShortest := 0
+	for _, galaxy := range galaxies {
+		// closest := findClosest(galaxies, galaxy)
+		for _, g2 := range galaxies {
+			sumShortest += distance(galaxy, g2)
+		}
+		// fmt.Println("Closest to", galaxy, "is", closest)
+	}
+
+	return sumShortest / 2
+}
+
+func findClosest(galaxies []Galaxy, galaxy Galaxy) int {
+	minDistance := -1
+	for _, g := range galaxies {
+		distance := distance(galaxy, g)
+		if distance > 0 {
+			if minDistance < 0 || distance < minDistance {
+				minDistance = distance
+			}
+		}
+	}
+	return minDistance
+}
+
+func distance(g1, g2 Galaxy) int {
+	return abs(g2.row-g1.row) + abs(g2.col-g1.col)
+}
+
+func abs(n int) int {
+	if n < 0 {
+		return -n
+	} else {
+		return n
+	}
+}
+
+func createGridUniverse(input []string) [][]byte {
+	// init
+	grid := [][]byte{}
+	for i := range input {
+		row := []byte(input[i])
+		grid = append(grid, row)
+	}
+
+	// expand rows
+	emptyRowIndexes := []int{}
+	for i := range grid {
+		if isEmptyRow(grid, i) {
+			emptyRowIndexes = append(emptyRowIndexes, i)
+		}
+	}
+	for _, i := range utils.ReverseList(emptyRowIndexes) {
+		emptyRow := grid[i]
+		for n := 0; n < 1000000-1; n++ {
+			if len(grid) == i {
+				grid = append(grid, emptyRow)
+			} else {
+				tmpGrid := append(grid[:i], emptyRow)
+				grid = append(tmpGrid, grid[i:]...)
+			}
+		}
+	}
+
+	// expand cols
+	emptyColIndexes := []int{}
+	for j := range grid[0] {
+		if isEmptyCol(grid, j) {
+			emptyColIndexes = append(emptyColIndexes, j)
+		}
+	}
+	newGrid := [][]byte{}
+	for _, row := range grid {
+		newRow := []byte{}
+		for j, b := range row {
+			newRow = append(newRow, b)
+			if utils.Contains(emptyColIndexes, j) {
+				for n := 0; n < 1000000-1; n++ {
+					newRow = append(newRow, b)
+				}
+			}
+		}
+		newGrid = append(newGrid, newRow)
+	}
+
+	return newGrid
+}
+
+func isEmptyRow(mat [][]byte, rowIndex int) bool {
+	for _, b := range mat[rowIndex] {
+		if b != '.' {
+			return false
+		}
+	}
+	return true
+}
+
+func isEmptyCol(mat [][]byte, colIndex int) bool {
+	for _, row := range mat {
+		if row[colIndex] != '.' {
+			return false
+		}
+	}
+	return true
+}
+
+func printMat(mat [][]byte) {
+	for _, row := range mat {
+		for _, b := range row {
+			fmt.Printf("%c", b)
+		}
+		fmt.Println()
+	}
 }
